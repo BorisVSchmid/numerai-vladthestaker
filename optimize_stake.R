57eccd79b201591ea6a81a77c7d99cfa936e5527
Boris <git@boris.earth>
2023-08-20 14:53:32 +0200

    added an example how to change how far back you are interested in looking.
    Setting a maximum number of eras you want to look back helps with looking at "the current market regime", as well as helps in including models that would otherwise be excluded as Vlad ignores models that don't go back all the way.

--- optimize_stake.R
+++ optimize_stake.R
@@ -30,9 +30,32 @@ source("functions.R")
 
 # Vlad Input and Settings
 #
-NMR <- 5000                                                    # Set the amount of NMR you want to stake
+NMR <- 1000                                                    # Set the amount of NMR you want to stake
 model_df <- read_excel("Optimize-Me.xlsx")                     # Read in the list of model names and their start (and possibly end) round.
 model_df$`Starting Era` <- pmax(model_df$`Starting Era`,339)   # Ignore starting eras that pre-date the start of daily staking (round 339)
+
+# Use this section to set how far back you want to look in time. As Vlad excludes models that are missing data
+# compared to their peers, setting a later starting era let's you include more recently developed models, but 
+# at the cost of the sample size of your time series.
+#
+
+model_df$`Starting Era`
+# As an example for why you might want to set a threshold from which era onward you want to evaluate 
+# your models:
+#
+# In the below two lines I am telling Vlad only to consider models that have data from era 410 onward. 
+# From round 450, everything got harder for LGBM's in the recent tournament, but as Vlad cannot deal (yet)
+# with negative returns (it starts to give somewhat unstable predictions), the best I could do is to 
+# evaluate the example models from era 410 onward, for which on average some are still positive.
+#
+#model_df$`Starting Era` <- pmax(model_df$`Starting Era`,410)
+#model_df <- model_df[model_df$`Starting Era` <= 410,]
+#
+# Another reason to pick different Starting Era's is when not all your models go back very far in time. More
+# recent models would get excluded by Vlad in favor for longer time series, but by setting the Starting Era 
+# to the era that your recent models have data from, they get included into Vlad's calculations (at the price
+# of having less eras in total to consider).
+#
 daily_data <- build_RAW(model_df,onlyresolved = TRUE)          # Choose whether to include only resolved rounds or not (TRUE/FALSE)
 
 
@@ -68,6 +91,9 @@ ggsave("model-performances-tc.png",scale=1,width=10,height=10)
 good_models_corr <- plot_corr[plot_corr$mean > 0,]$name
 good_models_tc <- plot_tc[plot_tc$mean > 0,]$name
 
+# You will get this error when there are no good-enough models.
+# Solver set to solveRquadprog
+# Error: Initialize timeSeries : length of '@units' not equal to '@.Data' extent
 
 
 # Build portfolios.
@@ -154,3 +180,46 @@ portfolio$tc_weight <- round(portfolio$tc_weight,3)
 kable(portfolio,caption = "Suggested portfolio. For models with both a _corr and a _tc component, either set up two model slots, or find a multiplier that works for both stakes")
 kable(singular,caption = "Expected returns based on separated weights")
 
+
+# From era 339 onwards.
+# 
+# Table: Suggested portfolio. For models with both a _corr and a _tc component, either set up two model slots, or find a multiplier that works for both stakes
+# 
+#   |name                | corr_weight| tc_weight| corr_0.5x| corr_1x| tc_0.5x| tc_1x| tc_1.5x| tc_2x| tc_2.5x| tc_3x|
+#   |:-------------------|-----------:|---------:|---------:|-------:|-------:|-----:|-------:|-----:|-------:|-----:|
+#   |INTEGRATION_TEST    |       0.810|     0.001|      1620|     810|       0|     0|       0|     0|       0|     0|
+#   |LG_LGBM_V4_JEROME20 |       0.024|     0.065|        50|      20|     130|    70|      40|    30|      30|    20|
+#   |LG_LGBM_V4_VICTOR20 |       0.095|     0.000|       190|      90|       0|     0|       0|     0|       0|     0|
+# 
+# 
+# Table: Expected returns based on separated weights
+# 
+#   |                       |   mean|    Cov|   CVaR|    VaR| samplesize|
+#   |:----------------------|------:|------:|------:|------:|----------:|
+#   |portfolio              | 0.0073| 0.0194| 0.0261| 0.0223|        191|
+#   |portfolio1_tangency    | 0.0076| 0.0198| 0.0269| 0.0240|        191|
+#   |portfolio2_minvariance | 0.0070| 0.0193| 0.0261| 0.0220|        191|
+#   |INTEGRATION_TEST       | 0.0059| 0.0161| 0.0212| 0.0176|        191|
+#   |LG_LGBM_V4_JEROME20    | 0.0006| 0.0022| 0.0036| 0.0029|        191|
+#   |LG_LGBM_V4_VICTOR20    | 0.0009| 0.0023| 0.0042| 0.0039|        191|
+
+
+# From era 410 onwards. Less data, but closer to the burn that started from week 450.
+#
+# Table: Suggested portfolio. For models with both a _corr and a _tc component, either set up two model slots, or find a multiplier that works for both stakes
+# 
+#   |name                | corr_weight| tc_weight| corr_0.5x| corr_1x| tc_0.5x| tc_1x| tc_1.5x| tc_2x| tc_2.5x| tc_3x|
+#   |:-------------------|-----------:|---------:|---------:|-------:|-------:|-----:|-------:|-----:|-------:|-----:|
+#   |LG_LGBM_V4_JEROME20 |       0.886|         0|      1770|     890|       0|     0|       0|     0|       0|     0|
+#   |LG_LGBM_V4_WALDO20  |       0.114|         0|       230|     110|       0|     0|       0|     0|       0|     0|
+# 
+# 
+# Table: Expected returns based on separated weights
+# 
+#   |                       |  mean|    Cov|   CVaR|    VaR| samplesize|
+#   |:----------------------|-----:|------:|------:|------:|----------:|
+#   |portfolio              | 8e-04| 0.0212| 0.0376| 0.0340|        122|
+#   |portfolio1_tangency    | 9e-04| 0.0212| 0.0378| 0.0342|        122|
+#   |portfolio2_minvariance | 7e-04| 0.0213| 0.0375| 0.0342|        122|
+#   |LG_LGBM_V4_JEROME20    | 8e-04| 0.0187| 0.0334| 0.0303|        122|
+#   |LG_LGBM_V4_WALDO20     | 0e+00| 0.0026| 0.0045| 0.0043|        122|
