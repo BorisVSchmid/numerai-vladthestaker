beec6144645c19a2f1104213fa66bb9ce1bd04a5
Boris <signup@boris.earth>
2023-11-27 20:45:49 +0100

    updated for the 0.5x mmc 2xcorr change.

--- optimize_stake.R
+++ optimize_stake.R
@@ -42,7 +42,7 @@ oldest_round <- min(model_df$start)
 ## Collect daily scores, and filter for models that have at least 60 data points (you shouldn't use Vlad for models with less data points)
 #  I have set the starting round at 339, as that is the first round of the daily tournament, but you are free to change that round back and forth.
 #
-daily_data <- build_RAW(model_df, MinfromRound = 339, corr_multiplier = 0, mmc_multiplier = 1)
+daily_data <- build_RAW(model_df, MinfromRound = 339, corr_multiplier = 0.5, mmc_multiplier = 2)
 daily_data <- daily_data[,colnames(daily_data) %in% colnames(daily_data)[colSums(!is.na(daily_data)) > 60]]
 
 
@@ -65,7 +65,7 @@ model_stats <- model_stats %>% mutate(sharpe = mean / sd,
 model_stats$scaled <- unlist(lapply(unlist((lapply(model_stats$tSSR,max,0.01))),sqrt))
 ggplot(model_stats) + 
   geom_point(aes(y=mean,x=sd, size=scaled, fill = drawdown),color="black",shape=21) + 
-  scale_fill_gradient(low="green",high="red") +
+  scale_fill_gradient2(low="green",mid="red",high="black",midpoint=0.9) +
   labs(size = "tSSR Sharpe") +
   geom_text_repel(aes(y=mean,x=sd,label=name),box.padding = 0.6,max.overlaps = 15, force_pull = 0.7,min.segment.length = 1.5)
 ggsave("model-performances.png",scale=1,width=15,height=15)
@@ -78,10 +78,12 @@ ggsave("model-performances.png",scale=1,width=15,height=15)
 # and what their stake-weighted drawdown is. I put the thresholds a little below that mean and a little above that drawdown.
 #
 # numerai_perf <- left_join(model_df,dplyr::select(model_stats,name,mean,drawdown)) %>% na.omit()
-# sum(numerai_perf$mean * numerai_perf$notes) / sum(numerai_perf$notes) # 0.00267 weighted mean
-# sum(numerai_perf$drawdown * numerai_perf$notes) / sum(numerai_perf$notes) # 0.53 weighted drawdown
+# sum(numerai_perf$mean * numerai_perf$notes) / sum(numerai_perf$notes) # 0.00846 weighted mean
+benchmark_mean <- 0.00846
+# sum(numerai_perf$drawdown * numerai_perf$notes) / sum(numerai_perf$notes) # 0.829 weighted drawdown
+benchmark_drawdown <- 0.829
 #
-good_models <- model_stats %>% dplyr::filter(mean > 0.0025, drawdown < 0.6)
+good_models <- model_stats %>% dplyr::filter(mean > benchmark_mean * 0.8, drawdown < benchmark_drawdown / 0.8) # Tweak as you feel is suited.
 
 
 
@@ -90,80 +92,72 @@ good_models <- model_stats %>% dplyr::filter(mean > 0.0025, drawdown < 0.6)
 starting_points <- unique(good_models %>% pull(first_round))
 table(starting_points)
 
-
-
-## Calculate portfolios from each of the starting points onwards.
-#
-# use threshold to remove small stakes from the portfolios (this limits the
-# final number of models you would have to stake on)
-threshold <- 0.1
-#
-combined <- data.frame()
-for (point in starting_points) {
-  print(point)
-  tryCatch({
-    relevant_models <- dplyr::filter(good_models,first_round <= point) %>% pull(name)
-    daily <- daily_data %>% dplyr::filter(roundNumber >= point) %>% dplyr::select(all_of(relevant_models)) %>% na.omit()
-    portfolio <- build_portfolio(daily,threshold = threshold)
-    newreturns <- as.data.frame(unlist(virtual_returns(daily_data,portfolio)))
-    merged <- cbind(portfolio,newreturns)
-    merged$starting_round <- point
-    merged[2:nrow(merged),4:9] <- ""
-    combined <- rbind(combined,merged,"")
-  }, error = function(e) {
-    message("An error occurred: ", e$message)
-  })
-}
-
-
-
-## Calculate merged portfolio (stake on this, submit a metapopulation model weighted to this.)
-#
-condensed <- combined %>% dplyr::filter(name != "") %>% dplyr::select(name,weight,stake)
-condensed$weight <- as.numeric(condensed$weight)
-condensed$stake <- as.numeric(condensed$stake)
-condensed <- condensed %>% group_by(name) %>% summarise(weight = sum(weight)/sum(condensed$weight), stake = sum(stake)/sum(condensed$weight))
-condensed_returns <- as.data.frame(unlist(virtual_returns(daily_data,condensed)))
-condensed <- cbind(condensed,condensed_returns)
-condensed[2:nrow(condensed),4:8] <- ""
-condensed$stake <- round(condensed$stake)
-
+  
+  
+  ## Calculate portfolios from each of the starting points onwards.
+  #
+  # use threshold to remove small stakes from the portfolios (this limits the
+  # final number of models you would have to stake on)
+  threshold <- 0.1
+  #
+  combined <- data.frame()
+  for (point in starting_points) {
+    print(point)
+    tryCatch({
+      relevant_models <- dplyr::filter(good_models,first_round <= point) %>% pull(name)
+      daily <- daily_data %>% dplyr::filter(roundNumber >= point) %>% dplyr::select(all_of(relevant_models)) %>% na.omit()
+      portfolio <- build_portfolio(daily,threshold = threshold)
+      newreturns <- as.data.frame(unlist(virtual_returns(daily_data,portfolio)))
+      merged <- cbind(portfolio,newreturns)
+      merged$starting_round <- point
+      merged[2:nrow(merged),4:9] <- ""
+      combined <- rbind(combined,merged,"")
+    }, error = function(e) {
+      message("An error occurred: ", e$message)
+    })
+  }
+  
+  
+  
+  ## Calculate merged portfolio (stake on this, submit a metapopulation model weighted to this.)
+  #
+  condensed <- combined %>% dplyr::filter(name != "") %>% dplyr::select(name,weight,stake)
+  condensed$weight <- as.numeric(condensed$weight)
+  condensed$stake <- as.numeric(condensed$stake)
+  condensed <- condensed %>% group_by(name) %>% summarise(weight = sum(weight)/sum(condensed$weight), stake = sum(stake)/sum(condensed$weight))
+  condensed_returns <- as.data.frame(unlist(virtual_returns(daily_data,condensed)))
+  condensed <- cbind(condensed,condensed_returns)
+  condensed[2:nrow(condensed),4:8] <- ""
+  condensed$stake <- round(condensed$stake)
+  
 
 
 ## Printout row-concatenated portfolios for different starting points.
 #
 kable(combined,digits=3)
 
+#   |name             |weight |stake |mean   |Cov    |CVaR   |VaR    |samplesize |starting_round |
+#   |:----------------|:------|:-----|:------|:------|:------|:------|:----------|:--------------|
+#   |INTEGRATION_TEST |0.412  |59    |0.0128 |0.0219 |0.0216 |0.0184 |273        |339            |
+#   |V4_LGBM_VICTOR20 |0.458  |65    |       |       |       |       |           |               |
+#   |V4_LGBM_VICTOR60 |0.13   |19    |       |       |       |       |           |               |
+#   |                 |       |      |       |       |       |       |           |               |
+#   |INTEGRATION_TEST |0.416  |59    |0.0128 |0.0218 |0.0215 |0.0182 |273        |340            |
+#   |V4_LGBM_VICTOR20 |0.46   |66    |       |       |       |       |           |               |
+#   |V4_LGBM_VICTOR60 |0.124  |18    |       |       |       |       |           |               |
+#   |                 |       |      |       |       |       |       |           |               |
 
-#   |name               |weight |stake |mean   |Cov    |CVaR   |VaR    |samplesize |starting_round |
-#   |:------------------|:------|:-----|:------|:------|:------|:------|:----------|:--------------|
-#   |INTEGRATION_TEST   |0.297  |42    |0.0043 |0.0078 |0.0078 |0.0066 |273        |339            |
-#   |V42_RAIN_ENSEMBLE2 |0.098  |14    |       |       |       |       |           |               |
-#   |V4_LGBM_NOMI20     |0.093  |13    |       |       |       |       |           |               |
-#   |V4_LGBM_TYLER60    |0.133  |19    |       |       |       |       |           |               |
-#   |V4_LGBM_VICTOR20   |0.379  |54    |       |       |       |       |           |               |
-#   |                   |       |      |       |       |       |       |           |               |
-#   |INTEGRATION_TEST   |0.236  |34    |0.004  |0.0074 |0.009  |0.0065 |272        |340            |
-#   |V2_EXAMPLE_PREDS   |0.225  |32    |       |       |       |       |           |               |
-#   |V42_RAIN_ENSEMBLE2 |0.087  |12    |       |       |       |       |           |               |
-#   |V4_LGBM_NOMI20     |0.08   |11    |       |       |       |       |           |               |
-#   |V4_LGBM_TYLER60    |0.073  |10    |       |       |       |       |           |               |
-#   |V4_LGBM_VICTOR20   |0.299  |43    |       |       |       |       |           |               |
-#   |                   |       |      |       |       |       |       |           |               |
 
 ## Printout combined portfolio across starting rounds (equal weight for each starting points for now)
+#  For Numre.ai's models, the two timepoints (starting_round 339 and 340) don't matter much.
 #
 kable(condensed,digits=3)
-# 
-# 
-#   |name               | weight| stake|mean   |Cov    |CVaR   |VaR    |samplesize |
-#   |:------------------|------:|-----:|:------|:------|:------|:------|:----------|
-#   |INTEGRATION_TEST   |  0.266|    38|0.0041 |0.0073 |0.0078 |0.0053 |272        |
-#   |V2_EXAMPLE_PREDS   |  0.112|    16|       |       |       |       |           |
-#   |V42_RAIN_ENSEMBLE2 |  0.092|    13|       |       |       |       |           |
-#   |V4_LGBM_NOMI20     |  0.086|    12|       |       |       |       |           |
-#   |V4_LGBM_TYLER60    |  0.103|    14|       |       |       |       |           |
-#   |V4_LGBM_VICTOR20   |  0.339|    48|       |       |       |       |           |
+
+#   |name             | weight| stake|mean   |Cov    |CVaR   |VaR    |samplesize |
+#   |:----------------|------:|-----:|:------|:------|:------|:------|:----------|
+#   |INTEGRATION_TEST |  0.414|    59|0.0128 |0.0218 |0.0216 |0.0183 |273        |
+#   |V4_LGBM_VICTOR20 |  0.459|    66|       |       |       |       |           |
+#   |V4_LGBM_VICTOR60 |  0.127|    18|       |       |       |       |           |
 
 
 
@@ -176,7 +170,8 @@ numerai_stake$weight <- numerai_stake$stake / sum(numerai_stake$stake)
 # You can calculate its return by feeding virtual_returns the daily_data and a dataframe of c(name, weight)
 kable(virtual_returns(daily_data,numerai_stake), digits=4)
 
-#   |   mean|   Cov|   CVaR|    VaR| samplesize|
-#   |------:|-----:|------:|------:|----------:|
-#   | 0.0027| 0.009| 0.0148| 0.0122|        272|
-   
+#   |   mean|    Cov|   CVaR|    VaR| samplesize|
+#   |------:|------:|------:|------:|----------:|
+#   | 0.0086| 0.0267| 0.0444| 0.0413|        272|
+
+# So, using Vlad suggests a higher account-wide return is possible against a lower covariance/CVaR and VaR. Time for stake management? :-).
